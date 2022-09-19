88cee8c75cfce50ffa8d75c50ce4c02c9fc74b79
Robin Elrharbi--Fleury <robin.elrharbifleury@gmail.com>
2022-09-19 16:28:42 +0100

    format day2

--- lib/day02.ml
+++ lib/day02.ml
@@ -91,17 +91,14 @@ module T : sig
 end = struct
   let name () = print_endline "--- Day 2: 1202 Program Alarm ---"
 
-    (* printf "op1 %d %d" pos destination;
-    print_s (Array.sexp_of_t Int.sexp_of_t input); *)
-
-  let op1 pos input = 
+  let op1 pos input =
     let first = Array.get input (Array.get input (pos + 1)) in
     let second = Array.get input (Array.get input (pos + 2)) in
     let destination = Array.get input (pos + 3) in
     Array.set input destination (first + second)
   ;;
 
-  let op2 pos input = 
+  let op2 pos input =
     let first = Array.get input (Array.get input(pos + 1)) in
     let second = Array.get input (Array.get input(pos + 2)) in
     let destination = Array.get input (pos + 3) in
@@ -112,7 +109,7 @@ end = struct
     let op1 pos = op1 pos input in
     let op2 pos = op2 pos input in
     let opcode = Array.get input pos in
-    let pos = 
+    let pos =
       match opcode with
         | 1 -> op1 pos; pos + 4;
         | 2 -> op2 pos; pos + 4;
@@ -125,89 +122,85 @@ end = struct
     in solve
   ;;
 
-  let part1 () =
+  let check_candidate noun verb =
     let input = Array.of_list input in
     let solve = make_solver input in
     let restore_1202_state () =
-      Array.set input 1 12;
-      Array.set input 2 2;
+      Array.set input 1 noun;
+      Array.set input 2 verb;
     in
     restore_1202_state ();
     solve 0;
-    printf "part1:\t%i\n" (Array.get input 0)
+    Array.get input 0
+  ;;
+
+  let part1 () =
+    let word = check_candidate 12 2 in
+    printf "part1:\t%i\n" (word)
   ;;
 
   (* "Good, the new computer seems to be working correctly! Keep it nearby
   during this mission - you'll probably use it again. Real Intcode computers
   support many more features than your new one, but we'll let you know what they
   are as you need them."
-  
+
   "However, your current priority should be to complete your gravity assist
   around the Moon. For this mission to succeed, we should settle on some
   terminology for the parts you've already built."
-  
+
   Intcode programs are given as a list of integers; these values are used as the
   initial state for the computer's memory. When you run an Intcode program, make
   sure to start by initializing memory to the program's values. A position in
   memory is called an address (for example, the first value in memory is at
   "address 0").
-  
+
   Opcodes (like 1, 2, or 99) mark the beginning of an instruction. The values
   used immediately after an opcode, if any, are called the instruction's
   parameters. For example, in the instruction 1,2,3,4, 1 is the opcode; 2, 3,
   and 4 are the parameters. The instruction 99 contains only an opcode and has
   no parameters.
-  
+
   The address of the current instruction is called the instruction pointer; it
   starts at 0. After an instruction finishes, the instruction pointer increases
   by the number of values in the instruction; until you add more instructions to
   the computer, this is always 4 (1 opcode + 3 parameters) for the add and
   multiply instructions. (The halt instruction would increase the instruction
   pointer by 1, but it halts the program instead.)
-  
+
   "With terminology out of the way, we're ready to proceed. To complete the
   gravity assist, you need to determine what pair of inputs produces the output
   19690720."
-  
+
   The inputs should still be provided to the program by replacing the values at
   addresses 1 and 2, just like before. In this program, the value placed in
   address 1 is called the noun, and the value placed in address 2 is called the
   verb. Each of the two input values will be between 0 and 99, inclusive.
-  
+
   Once the program has halted, its output is available at address 0, also just
   like before. Each time you try a pair of inputs, make sure you first reset the
   computer's memory to the values in the program (your puzzle input) - in other
   words, don't reuse memory from a previous attempt.
-  
+
   Find the input noun and verb that cause the program to produce the output
   19690720. What is 100 * noun + verb? (For example, if noun=12 and verb=2, the
   answer would be 1202.) *)
-  
+
   let part2 () =
-    let check_candidate noun verb =  
-      let input = Array.of_list input in
-      let solve = make_solver input in
-      let restore_1202_state () =
-        Array.set input 1 noun;
-        Array.set input 2 verb;
-      in
-      restore_1202_state ();
-      solve 0;
-      Array.get input 0
-    in
-    let rec try_all noun verb = 
+    let rec try_all noun verb =
       let x = check_candidate noun verb in
-      match x = 19690720 with 
+      let try_next () =
+        match try_all (noun + 1) verb with
+          | Some x -> Some x
+          | None -> try_all noun (verb + 1)
+      in
+      match x = 19690720 with
         | true -> Some (100 * noun + verb)
         | false -> match x > 19690720 with
             | true -> None
-            | false -> (
-              match (try_all (noun + 1) verb) with 
-                | Some x -> Some x
-                | None -> try_all (noun) (verb + 1))
+            | false -> try_next ()
     in
     let word = Option.value_exn (try_all 12 2) in
     printf "part2:\t%i\n" (word)
-    ;;
-  
+  ;;
+
 end
