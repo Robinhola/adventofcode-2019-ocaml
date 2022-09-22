6ac073ab9b4dd0cff5a5efa554648c02ed178dd6
Robin Elrharbi--Fleury <robin.elrharbifleury@gmail.com>
2022-09-22 07:00:45 +0100

    Day 3 - part 2

--- lib/day03.ml
+++ lib/day03.ml
@@ -93,32 +93,34 @@ end = struct
   printf "%d %d" (Coord.distance t1) (Coord.distance t2);
   [%expect {| 10 10 |}]
 
+  let rec make_move history position ~direction ~move ~count =
+    let px, py = position in
+    let dx, dy = direction in 
+    match move with 
+    | 0 -> history, position, count
+    | _ -> (
+      let move = move - 1 in 
+      let count = count + 1 in 
+      let position = px + dx, py + dy in 
+      let history = Map.add_multi history ~key:position ~data:count in
+      make_move history position ~direction ~move ~count
+    )
+  ;;
+
+  let rec move_wire history position moves count =
+    match moves with 
+    | [] -> history
+    | m :: moves -> 
+      let direction = Coord.from_char (Char.of_string(String.sub m ~pos:0 ~len:1)) in
+      let move = Int.of_string (String.sub m ~pos:1 ~len:((String.length m) - 1)) in 
+      let history, position, count = make_move history position ~direction ~move ~count in
+      move_wire history position moves count;
+  ;;
 
   let part1 () =
-    let rec make_move history position ~direction ~move =
-      let px, py = position in
-      let dx, dy = direction in 
-      match move with 
-      | 0 -> history, position
-      | _ -> (
-        let position = px + dx, py + dy in 
-        let history = Map.add_multi history ~key:position ~data:true in
-        let move = move - 1 in 
-        make_move history position ~direction ~move
-      )
-    in 
-    let rec move_wire history position moves =
-      match moves with 
-      | [] -> history
-      | m :: moves -> 
-        let direction = Coord.from_char (Char.of_string(String.sub m ~pos:0 ~len:1)) in
-        let move = Int.of_string (String.sub m ~pos:1 ~len:((String.length m) - 1)) in 
-        let history, position = make_move history position ~direction ~move in
-        move_wire history position moves;
-    in
     let find_wire_positions id = 
       let wire_moves = List.nth_exn lines id in 
-      let wire_positions  = Map.key_set (move_wire CoordMap.empty (0, 0) wire_moves) in
+      let wire_positions  = Map.key_set (move_wire CoordMap.empty (0, 0) wire_moves 0) in
       wire_positions
     in
     let intersections = Set.to_list (Set.inter (find_wire_positions 0) (find_wire_positions 1)) in
@@ -126,8 +128,63 @@ end = struct
     printf "part1:\t%i\n" (Coord.distance (Option.value_exn closest_intersection))
   ;;
 
+  (* --- Part Two ---
+  It turns out that this circuit is very timing-sensitive; you actually need to
+  minimize the signal delay.
+  
+  To do this, calculate the number of steps each wire takes to reach each
+  intersection; choose the intersection where the sum of both wires' steps is
+  lowest. If a wire visits a position on the grid multiple times, use the steps
+  value from the first time it visits that position when calculating the total
+  value of a specific intersection.
+  
+  The number of steps a wire takes is the total number of grid squares the wire
+  has entered to get to that location, including the intersection being
+  considered. Again consider the example from above:
+
+  ...........
+  .+-----+...
+  .|.....|...
+  .|..+--X-+.
+  .|..|..|.|.
+  .|.-X--+.|.
+  .|..|....|.
+  .|.......|.
+  .o-------+.
+  ...........
+  In the above example, the intersection closest to the central port is reached
+  after 8+5+5+2 = 20 steps by the first wire and 7+6+4+3 = 20 steps by the
+  second wire for a total of 20+20 = 40 steps.
+  
+  However, the top-right intersection is better: the first wire takes only 8+5+2
+  = 15 and the second wire takes only 7+6+2 = 15, a total of 15+15 = 30 steps.
+  
+  Here are the best steps for the extra examples from above:
+  
+  R75,D30,R83,U83,L12,D49,R71,U7,L72
+  U62,R66,U55,R34,D71,R55,D58,R83 = 610 steps
+  R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51
+  U98,R91,D20,R16,D67,R40,U7,R15,U6,R7 = 410 steps
+
+  What is the fewest combined steps the wires must take to reach an
+  intersection?
+  *)
+
   let part2 () =
-    printf "part2:\t%i\n" (0)
+    (* let lines = [["R75";"D30";"R83";"U83";"L12";"D49";"R71";"U7";"L72"]; ["U62";"R66";"U55";"R34";"D71";"R55";"D58";"R83"]] in  *)
+    (* let lines = [["R98";"U47";"R26";"D63";"R33";"U87";"L62";"D20";"R33";"U53";"R51"]; ["U98";"R91";"D20";"R16";"D67";"R40";"U7";"R15";"U6";"R7"]] in  *)
+    let find_wire_positions id = 
+      let wire_moves = List.nth_exn lines id in 
+      Map.map
+        (move_wire CoordMap.empty (0, 0) wire_moves 0)
+        ~f:(fun l -> Option.value_exn (List.min_elt l ~compare:Int.compare))
+    in
+    let first = find_wire_positions 0 in 
+    let second = find_wire_positions 1 in
+    let intersections = Set.to_list (Set.inter (Map.key_set first) (Map.key_set second)) in
+    let distance c = (Map.find_exn first c) + (Map.find_exn second c) in
+    let closest_intersection = List.min_elt intersections ~compare:(fun a b -> distance a - distance b) |> Option.value_exn in
+    printf "part2:\t%i\n" (distance closest_intersection);
   ;;
 
 end
