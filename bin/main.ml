8f6945e90c15241d781845477978a5b39959a398
Robin Elrharbi--Fleury <robin.elrharbifleury@gmail.com>
2022-10-01 15:31:47 +0100

    First class modules

--- bin/main.ml
+++ bin/main.ml
@@ -3,18 +3,14 @@ open! Core
 
 let () = print_endline "Welcome to 2019! Let the advent of code... begin!!!"
 
-let () = Advent_2019.Day01.T.name ()
-let () = Advent_2019.Day01.T.part1 ()
-let () = Advent_2019.Day01.T.part2 ()
+let solve (module M: Advent_2019.Day.T) = 
+  let () = M.name () in 
+  let () = M.part1 () in 
+  let () = M.part2 () in 
+  ()
+;;
 
-let () = Advent_2019.Day02.T.name ()
-let () = Advent_2019.Day02.T.part1 ()
-let () = Advent_2019.Day02.T.part2 ()
-
-let () = Advent_2019.Day03.T.name ()
-let () = Advent_2019.Day03.T.part1 ()
-let () = Advent_2019.Day03.T.part2 ()
-
-let () = Advent_2019.Day04.T.name ()
-let () = Advent_2019.Day04.T.part1 ()
-let () = Advent_2019.Day04.T.part2 ()
+let () = solve (module Advent_2019.Day01.T)
+let () = solve (module Advent_2019.Day02.T)
+let () = solve (module Advent_2019.Day03.T)
+let () = solve (module Advent_2019.Day04.T)
