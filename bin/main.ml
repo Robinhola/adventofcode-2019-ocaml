d7467340a4d5282183e127120c3947238dea2eda
Robin Elrharbi--Fleury <robin.elrharbifleury@gmail.com>
2022-12-04 18:10:43 +0530

    day5!

--- bin/main.ml
+++ bin/main.ml
@@ -1,16 +1,18 @@
 open! Base
 open! Core
+open Advent_2019
 
 let () = print_endline "Welcome to 2019! Let the advent of code... begin!!!"
 
-let solve (module M: Advent_2019.Day.T) = 
+let solve (module M: Day.T) = 
   let () = M.name () in 
   let () = M.part1 () in 
   let () = M.part2 () in 
   ()
 ;;
 
-let () = solve (module Advent_2019.Day01.T)
-let () = solve (module Advent_2019.Day02.T)
-let () = solve (module Advent_2019.Day03.T)
-let () = solve (module Advent_2019.Day04.T)
+let () = solve (module Day01.T)
+let () = solve (module Day02.T)
+let () = solve (module Day03.T)
+let () = solve (module Day04.T)
+let () = solve (module Day05.T)
